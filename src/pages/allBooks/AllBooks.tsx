5017c481cef3b9efa6a24aa76cff049d2a2d683e
Rashadul-Islam <rashadulislam4581@gmail.com>
2023-07-16 13:16:37 +0600

    add pagination

--- src/pages/allBooks/AllBooks.tsx
+++ src/pages/allBooks/AllBooks.tsx
@@ -3,11 +3,16 @@ import Footer from "@/layouts/Footer";
 import Navbar from "@/layouts/Navbar";
 import { useGetBooksQuery } from "@/redux/features/books/bookApi";
 import { IBook } from "@/types/bookType";
+import { Pagination } from "@mui/material";
 import { useState } from "react";
 
 const AllBooks = () => {
   const [serachData, setSearchData] = useState<string>("");
-  const { data } = useGetBooksQuery(`?searchTerm=${serachData}`);
+  const [page, setPage] = useState<number>(1);
+  const { data } = useGetBooksQuery(`?searchTerm=${serachData}&page=${page}`);
+  const handleChange = (event, value: number) => {
+    setPage(value);
+  };
   return (
     <div>
       <Navbar />
@@ -17,13 +22,23 @@ const AllBooks = () => {
             type="text"
             placeholder="Search by title, author, genre..."
             onChange={(e) => setSearchData(e.target.value)}
-            className="w-[40%] px-4 py-2 rounded-full placeholder:text-black bg-gray-100 text-black focus:outline-none focus:ring-2 focus:ring-gray-900"
+            className="lg:w-[40%] md:w-[60] w-[90%] px-4 py-2 rounded-full placeholder:text-black bg-gray-100 text-black focus:outline-none focus:ring-2 focus:ring-gray-900"
           />
         </div>
         <div className="flex flex-wrap justify-between">
           {data?.data?.map((book: IBook) => (
             <Book key={book._id} book={book} />
           ))}
+        </div>
+        <div className="flex justify-center pt-10">
+          <Pagination
+            count={data && Math.ceil(data?.meta?.total / data?.meta?.limit)}
+            page={page}
+            onChange={handleChange}
+            variant="outlined"
+            shape="rounded"
+            color="primary"
+          />
         </div>
       </div>
       <Footer />
